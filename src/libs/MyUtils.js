89e991399148f87ed365ca92b45147e7269ab45d
Anthony S <tonesto7@gmail.com>
2020-02-01 12:34:38 -0500

    express now binds to all interfaces Cleanups Package updates

--- src/libs/MyUtils.js
+++ src/libs/MyUtils.js
@@ -44,11 +44,7 @@ module.exports = class MyUtils {
             let iface = interfaces[devName];
             for (let i = 0; i < iface.length; i++) {
                 let alias = iface[i];
-                if (
-                    alias.family === "IPv4" &&
-                    alias.address !== "127.0.0.1" &&
-                    !alias.internal
-                ) {
+                if (alias.family === "IPv4" && alias.address !== "127.0.0.1" && !alias.internal) {
                     return alias.address;
                 }
             }
